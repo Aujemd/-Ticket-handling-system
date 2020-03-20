10dab94237c724375875a857543ccc8557f6d868
Johan Mora <joenmodu99@gmail.com>
2020-03-19 22:01:10 -0400

    Commit Inicial

---
+++ public/index.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Aura\Router\RouterContainer;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load(); 
+
+if(getenv('DEBUG') == 'true'){
+    ini_set('display_errors', 1);
+    ini_set('display_starup_error', 1);
+    error_reporting(E_ALL);
+}
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$routerContainer = new RouterContainer();
+
+$request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
+
+$map = $routerContainer->getMap();
+
+$map->get('index', getenv('BASE_URL'), [
+    'controller' => 'App\Controllers\IndexController',
+    'action' => 'indexAction',
+]);
+
+$matcher = $routerContainer->getMatcher();
+
+$route = $matcher->match($request);
+
+if(!$route){
+    echo 'No Route';
+}else{
+    $handlerData = $route->handler;
+    $controllerName = $handlerData['controller'];
+    $actionName = $handlerData['action'];
+    $controller = new $controllerName;
+    $response = $controller->$actionName($request);
+
+    foreach($response->getHeaders() as $name => $values){//Mojon para que no se puteen las url al redireccionar las paginas
+        foreach($values as $value){
+            header(sprintf('%s: %s', $name, $value), false);
+        }
+    }
+
+    http_response_code($response->getStatusCode());
+    echo $response->getBody();
+}
