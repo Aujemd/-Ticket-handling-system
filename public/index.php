4e9ff74db00157ebfbc6411b41a8cb309532ecbe
Johan Mora <joenmodu99@gmail.com>
2020-03-30 00:55:27 -0400

    Arreglando driver de db

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ if(getenv('DEBUG') == 'true'){
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
+    'driver'    => getenv('DB_DRIVER'),
     'host'      => getenv('DB_HOST'),
     'database'  => getenv('DB_NAME'),
     'username'  => getenv('DB_USER'),
