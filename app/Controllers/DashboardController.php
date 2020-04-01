7aa87014a552326c0b3b3d80fc0ac6af6101b052
Johan Mora <joenmodu99@gmail.com>
2020-04-01 10:22:58 -0400

    Fix curly braces deprecated

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends BaseController{
             $key = '';
             $pattern = '1234567890abcdefghijklmnopqrstuvwxyz';
             $max = strlen($pattern)-1;
-            for($i=0;$i < $longitud;$i++) $key .= $pattern{mt_rand(0,$max)};
+            for($i=0;$i < $longitud;$i++) $key .= $pattern[mt_rand(0,$max)];
             return $key;
         }
 
